be66f0eebef5935155ccdf8b84b0e29a421b704b
Saurav <sp.gharti@gmail.com>
2023-02-02 03:29:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-snowflake-2",
+  url: "https://twilight-snowflake-2.botics.co",
+  api: "https://twilight-snowflake-2.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
